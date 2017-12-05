e5a22f18776a39d662dd51c1619561de85444650
David Corbacho <david.corbacho@mudano.com>
2017-12-05 17:42:13 +0000

    Add storybook addon Info, and config options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,21 @@
 import { configure } from '@storybook/react';
+import { setDefaults } from '@storybook/addon-info';
+
+// addon-info
+setDefaults({
+  header: false, // Toggles display of header with component name and description
+  inline: true, // Displays info inline vs click button to view
+  source: true, // Displays the source of story Component
+  propTables: [/* Components used in story */], // displays Prop Tables with this components
+  propTablesExclude: [], // Exclude Components from being shown in Prop Tables section
+  styles: {}, // Overrides styles of addon
+  marksyConf: {}, // Overrides components used to display markdown. Warning! This option's name will be likely deprecated in favor to "components" with the same API in 3.3 release. Follow this PR #1501 for details
+  maxPropsIntoLine: 1, // Max props to display per line in source code
+  maxPropObjectKeys: 10, // Displays the first 10 characters of the prop name
+  maxPropArrayLength: 10, // Displays the first 10 items in the default prop array
+  maxPropStringLength: 100, // Displays the first 100 characters in the default prop string
+});
+
 
 function loadStories() {
   require('../stories');
