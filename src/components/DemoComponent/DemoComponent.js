9c98e713263e3b69cd59f781c1c93c4dd74e3f46
Jakub Frej <jakub.frej@mudano.com>
2017-12-11 00:10:59 +0000

    👕 LINT: improve linting of DemoComponent, add missing jsx indent rule to eslintrc

--- src/components/DemoComponent/DemoComponent.js
+++ src/components/DemoComponent/DemoComponent.js
@@ -3,23 +3,30 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import './DemoComponent.css';
 
-const DemoComponent = ({title, children, isDisabled, onClick}) => (
-  <div className={cx('ui-demo-component', {'is-disabled': isDisabled})} onClick={onClick}>
-    <div className="ui-demo-component--title"><h2>{title}</h2></div>
-    <div className="ui-demo-component--content">{children}</div>
-  </div>
-)
+const DemoComponent = ({
+    title,
+    children,
+    isDisabled,
+    onClick,
+}) => (
+    <div className={cx('ui-demo-component', { 'is-disabled': isDisabled })} onClick={onClick}>
+        <div className="ui-demo-component--title"><h2>{title}</h2></div>
+        <div className="ui-demo-component--content">{children}</div>
+    </div>
+);
 
 DemoComponent.defaultProps = {
-    title: "Demo",
-    onClick: () => {}
+    children: null,
+    isDisabled: false,
+    onClick: () => { },
+    title: 'Demo',
 };
 
 DemoComponent.propTypes = {
     title: PropTypes.string,
     onClick: PropTypes.func,
     children: PropTypes.node,
-    isDisabled: PropTypes.bool
-}
+    isDisabled: PropTypes.bool,
+};
 
 export default DemoComponent;
--- src/components/DemoComponent/DemoComponent.story.js
+++ src/components/DemoComponent/DemoComponent.story.js
@@ -1,41 +1,41 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { withInfo } from "@storybook/addon-info";
-import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
-import DemoComponent from "./DemoComponent";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import DemoComponent from './DemoComponent';
 
-const stories = storiesOf("DemoComponent", module);
+const stories = storiesOf('DemoComponent', module);
 
 stories
-  .addDecorator(withKnobs)
-  .addDecorator(story => <div id="ui-root-preview">{story()}</div>);
+    .addDecorator(withKnobs)
+    .addDecorator(story => <div id="ui-root-preview">{story()}</div>);
 
 stories.add(
-  "default",
-  withInfo(`
+    'default',
+    withInfo(`
         With a custom title
     `)(() => (
-    <DemoComponent
-      isDisabled={boolean("isDisabled", false)}
-      title={text("title", "Custom title")}
-      onClick={action("clicked")}
-    >
-      children
-    </DemoComponent>
-  ))
+        <DemoComponent
+            isDisabled={boolean('isDisabled', false)}
+            title={text('title', 'Custom title')}
+            onClick={action('clicked')}
+        >
+        children
+        </DemoComponent>
+    ))
 );
 
 stories.add(
-  "with title",
-  withInfo(`
+    'with title',
+    withInfo(`
         With a custom title
     `)(() => <DemoComponent title="Custom title" />)
 );
 
 stories.add(
-  "with emoji",
-  withInfo()(() => (
-    <DemoComponent onClick={action("clicked")}>😀 😎 👍 💯</DemoComponent>
-  ))
+    'with emoji',
+    withInfo()(() => (
+        <DemoComponent onClick={action('clicked')}>😀 😎 👍 💯</DemoComponent>
+    ))
 );
