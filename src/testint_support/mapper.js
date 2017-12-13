b5fa03c6f86f93e2d3f10a75c82911d14e51accd
AbhijnaBekal <abhijna.bekal@mudano.com>
2017-12-13 09:16:56 +0000

    modifed `mapper.js` by including the right path for default button and button with right icon. Included `testint_Button.js` with tests for default button component and conponent with right icon

--- src/testint_support/mapper.js
+++ src/testint_support/mapper.js
@@ -4,5 +4,13 @@ module.exports = {
 
   demoComponent: {
       default: '/?selectedKind=DemoComponent&selectedStory=default'
+  },
+
+  buttonComponent: {
+      default: '/?selectedKind=Button&selectedStory=default'
+  },
+
+  buttonComponentRightIcon: {
+      righticon: '/?selectedKind=Button&selectedStory=right%20icon'
   }
 }
