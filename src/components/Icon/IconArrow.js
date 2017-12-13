778205e2a118298953dd345d3718e84e4d61019f
David Corbacho <david.corbacho@mudano.com>
2017-12-13 14:12:01 +0000

    Add style prop to IconArrow

--- src/components/Icon/IconArrow.js
+++ src/components/Icon/IconArrow.js
@@ -8,11 +8,13 @@ const IconArrow = ({
     width,
     height,
     className,
+    style,
 }) => (
     <svg
         className={cx('uir-icon uir-icon-arrow', className)}
         width={width}
         height={height}
+        style={style}
         viewBox="0 0 24 24"
     >
         <path fill="#2F2833" d="M12.778 14.72l5.657-5.656.707.707-5.657 5.657zM5 9.771l.707-.707 7.071 7.071-.707.707z" fillRule="evenodd"/>
@@ -23,12 +25,14 @@ IconArrow.defaultProps = {
     width: 24,
     height: 24,
     className: '',
+    style: {},
 };
 
 IconArrow.propTypes = {
     width: PropTypes.number,
     height: PropTypes.number,
-    className: '',
+    className: PropTypes.string,
+    style: PropTypes.objectOf(PropTypes.object),
 };
 
 export default IconArrow;
