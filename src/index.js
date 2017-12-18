8959906bcadea44bbe07f0c048407fe954266eb2
David Corbacho <david.corbacho@mudano.com>
2017-12-18 08:47:29 +0000

    export Button Components in the root index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import DemoComponent from './components/DemoComponent/DemoComponent';
+export { default as DemoComponent } from './components/DemoComponent/DemoComponent';
 
-export default DemoComponent;
+// Buttons
+export { default as Button } from './components/Button/Button';
+export { default as IconButton } from './components/Button/IconButton';
+export { default as PrimaryButton } from './components/Button/PrimaryButton';
+
+// Icons
+export { default as IconArrow } from './components/Icon/IconArrow';
