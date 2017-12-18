3d64f74e2f1ef16f7658e86718b51625a2a742e2
Jakub Frej <jakub.frej@mudano.com>
2017-12-18 12:45:27 +0000

    [MDU-44] ✅ TESTS: fix Button and IconButton tests to use chai assertions and sinon

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,6 +1,7 @@
-/* global describe, expect, it, jest, shallow */
+/* global describe, expect, it, shallow */
 import React from 'react';
-import * as common from '../../../test/jest/commonTests';
+import * as common from '../../../test/unit/commonTests';
+import { sandbox } from '../../../test/unit/utils';
 import Button from './Button';
 
 describe('Button', () => {
@@ -11,27 +12,27 @@ describe('Button', () => {
             <Button>Foo</Button>
         ));
 
-        expect(wrapper).toHaveTagName('button');
+        expect(wrapper).to.have.tagName('button');
     });
 
     it('Button should render with class .uir-Button', () => {
         const wrapper = shallow(<Button>Foo</Button>);
-        expect(wrapper.find('.uir-Button').length).toBe(1);
+        expect(wrapper.find('.uir-Button').length).to.equal(1);
     });
 
     it('Clear button should render with class .uir-Button-clear', () => {
-        const wrapper = shallow(<Button skin="clear">Foo</Button>);
-        expect(wrapper.find('.uir-Button-clear').length).toBe(1);
+        const wrapper = shallow(<Button isClear>Foo</Button>);
+        expect(wrapper.find('.uir-Button-clear').length).to.equal(1);
     });
 
     it('Disabled button should render with class .uir-Button-disabled', () => {
         const wrapper = shallow(<Button isDisabled>Foo</Button>);
-        expect(wrapper.find('.uir-Button-disabled').length).toBe(1);
+        expect(wrapper.find('.uir-Button-disabled').length).to.equal(1);
     });
 
     it('Active button should render with class .uir-Button-active', () => {
         const wrapper = shallow(<Button isActive>Foo</Button>);
-        expect(wrapper.find('.uir-Button-active').length).toBe(1);
+        expect(wrapper.find('.uir-Button-active').length).to.equal(1);
     });
 
     it('should render three Button components', () => {
@@ -42,8 +43,9 @@ describe('Button', () => {
                 <Button isDisabled>Foo</Button>
             </div>
         ));
-        expect(wrapper.find(Button).length).toBe(3);
+        expect(wrapper.find(Button).length).to.equal(3);
     });
+
     describe('onClick', () => {
         it('default does not throw when clicked', () => {
             const syntheticEvent = { preventDefault: () => undefined };
@@ -54,30 +56,31 @@ describe('Button', () => {
                 wrapper.simulate('click', syntheticEvent);
             };
 
-            expect(clickComponent).not.toThrow();
+            expect(clickComponent).not.to.throw();
         });
 
         it('is called when clicked', () => {
-            const onClick = jest.fn();
+            const onClick = sandbox.spy();
             const syntheticEvent = { preventDefault: () => undefined };
             const wrapper = shallow((
                 <Button onClick={onClick}>Foo</Button>
             ));
 
             wrapper.simulate('click', syntheticEvent);
-            expect(onClick.mock.calls.length).toEqual(1);
-            expect(onClick).toBeCalledWith(syntheticEvent);
+            expect(onClick).to.have.been.calledOnce();
+            expect(onClick).to.have.been.calledWithExactly(syntheticEvent);
         });
 
         it('is not called when clicked on Disable button', () => {
-            const onClick = jest.fn();
-            const preventDefault = jest.fn();
+            const onClick = sandbox.spy();
+            const preventDefault = sandbox.spy();
             const wrapper = shallow((
                 <Button isDisabled onClick={onClick}>Foo</Button>
             ));
+
             wrapper.simulate('click', { preventDefault });
-            expect(onClick).not.toHaveBeenCalledTimes(1);
-            expect(preventDefault).toHaveBeenCalledTimes(1);
+            expect(onClick).not.to.have.been.called();
+            expect(preventDefault).to.have.been.calledOnce();
         });
     });
 });
--- src/components/Button/IconButton.test.js
+++ src/components/Button/IconButton.test.js
@@ -1,5 +1,6 @@
-/* global describe, expect, it, jest, shallow */
+/* global describe, expect, it, shallow */
 import React from 'react';
+import { sandbox } from '../../../test/unit/utils';
 import IconButton from './IconButton';
 import IconArrow from '../Icon/IconArrow';
 
@@ -9,38 +10,38 @@ describe('IconButton', () => {
             <IconButton><IconArrow /></IconButton>
         )).dive();
 
-        expect(wrapper).toHaveTagName('button');
+        expect(wrapper).to.have.tagName('button');
     });
 
     it('Button should render with class .uir-IconButton', () => {
         const wrapper = shallow(<IconButton><IconArrow /></IconButton>).dive();
-        expect(wrapper.find('.uir-IconButton').length).toBe(1);
+        expect(wrapper.find('.uir-IconButton').length).to.equal(1);
     });
 
     it('Default IconButton should render with class .uir-IconButton', () => {
         const wrapper = shallow(<IconButton><IconArrow /></IconButton>).dive();
-        expect(wrapper.find('.uir-IconButton').length).toBe(1);
+        expect(wrapper.find('.uir-IconButton').length).to.equal(1);
     });
 
     it('Clear IconButton should render with custom class .custom-class', () => {
         const wrapper = shallow((
             <IconButton className="custom-class"><IconArrow /></IconButton>
         )).dive();
-        expect(wrapper.find('.custom-class').length).toBe(1);
+        expect(wrapper.find('.custom-class').length).to.equal(1);
     });
     it('Disabled IconButton should render with class .uir-Button-disabled', () => {
         const wrapper = shallow(<IconButton isDisabled><IconArrow /></IconButton>).dive();
-        expect(wrapper.find('.uir-Button-disabled').length).toBe(1);
+        expect(wrapper.find('.uir-Button-disabled').length).to.equal(1);
     });
 
     it('IconButton should not be fluid, even if isFluid flag is passed', () => {
         const wrapper = shallow(<IconButton isFluid><IconArrow /></IconButton>).dive();
-        expect(wrapper.find('.uir-Button-fluid').length).toBe(0);
+        expect(wrapper.find('.uir-Button-fluid').length).to.equal(0);
     });
 
     it('Active IconButton should render with class .uir-Button-active', () => {
         const wrapper = shallow(<IconButton isActive><IconArrow /></IconButton>).dive();
-        expect(wrapper.find('.uir-Button-active').length).toBe(1);
+        expect(wrapper.find('.uir-Button-active').length).to.equal(1);
     });
 
     describe('onClick', () => {
@@ -53,19 +54,19 @@ describe('IconButton', () => {
                 wrapper.simulate('click', syntheticEvent);
             };
 
-            expect(clickComponent).not.toThrow();
+            expect(clickComponent).not.to.throw();
         });
 
         it('is called when clicked', () => {
-            const onClick = jest.fn();
+            const onClick = sandbox.spy();
             const syntheticEvent = { preventDefault: () => undefined };
             const wrapper = shallow((
                 <IconButton onClick={onClick}><IconArrow /></IconButton>
             )).dive();
 
             wrapper.simulate('click', syntheticEvent);
-            expect(onClick.mock.calls.length).toEqual(1);
-            expect(onClick).toBeCalledWith(syntheticEvent);
+            expect(onClick).to.have.been.calledOnce();
+            expect(onClick).to.have.been.calledWithExactly(syntheticEvent);
         });
     });
 });
