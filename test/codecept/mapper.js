1360060d7bdd9b98610d7732567860460e53d20e
rizmudano <rizwan.khan@mudano.com>
2017-12-19 18:47:15 +0000

    removing obsolete URLs in mapper

--- test/codecept/mapper.js
+++ test/codecept/mapper.js
@@ -7,29 +7,9 @@ module.exports = {
     },
 
     buttonComponent: {
-<<<<<<< HEAD
         buttonDemo: '/?selectedKind=Button&selectedStory=demo',
         iconButtonDemo: '/?selectedKind=Button.IconButton&selectedStory=-demo',
         primaryButtonDemo: '/?selectedKind=Button.PrimaryButton&selectedStory=-demo',
-    },
-
-    buttonComponentRightLeftIcon: {
-        rightlefticon: '/?selectedKind=Button&selectedStory=right%20%26%20left%20icon',
-    },
-
-    iconButton: {
-        demo: '/?selectedKind=Button.PrimaryButton&selectedStory=-demo',
-=======
-        default: '/?selectedKind=Button&selectedStory=default',
-    },
-
-    buttonComponentRightIcon: {
-        righticon: '/?selectedKind=Button&selectedStory=right%20icon',
-    },
-
-    buttonComponentRightLeftIcon: {
-        rightlefticon: '/?selectedKind=Button&selectedStory=right%20%26%20left%20icon',
->>>>>>> 0a184307f5d3cfaafa3bc8ad372390d9a195b3b6
     },
 
 };
