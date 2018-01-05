7ff5169a41d72c70188708e98dbd0255def78898
Jakub Frej <jakub.frej@mudano.com>
2018-01-05 00:14:38 +0000

    :star: [MDU-44] Remove PrimaryButton import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ export { default as DemoComponent } from './components/DemoComponent/DemoCompone
 // Buttons
 export { default as Button } from './components/Button/Button';
 export { default as IconButton } from './components/Button/IconButton';
-export { default as PrimaryButton } from './components/Button/PrimaryButton';
 
 // Icons
 export { default as IconArrow } from './components/Icon/IconArrow';
