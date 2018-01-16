a2e79bd24f2aaed41bbf6ca743c6369e0ee59dcf
Jakub Frej <jakub.frej@mudano.com>
2018-01-16 23:44:22 +0000

    :white_check_mark: [MDU-44] Add better Button integration tests

--- src/components/Button/Button.test-int.js
+++ src/components/Button/Button.test-int.js
@@ -1,16 +1,48 @@
-/* global Feature, Scenario, within */
+/* global Feature, Scenario */
 // eslint-disable-next-line spaced-comment
 /// path="../../../test/codecept/steps.d.ts"
 
+const KIND = 'Button.Button';
+
 Feature('Button');
 
-Scenario('renders and is clickable', (I, mapper) => {
-    const buttonContainer = '.uir-button';
+Scenario('Storybook Documentation', (I) => {
+    I.searchWithinIframe(KIND, 'Overview', () => {
+        I.see('Button: Button');
+        I.see('Overview', 'h2');
+        I.see('Example', 'h3');
+        I.see('Source Code', 'h3');
+        I.seeElement('code');
+        I.see('Variations', 'h3');
+        I.see('Props', 'h3');
+        I.seeElement('table');
+        I.see('property');
+        I.see('value');
+        I.see('required');
+        I.see('default');
+        I.seeNumberOfElements('td', 13 * 4);
+    });
+});
 
-    I.amOnPage(mapper.buttonComponent.buttonDemo);
-    within({ frame: '#storybook-preview-iframe' }, () => {
-        I.see('CUSTOM LABEL', buttonContainer);
-        I.click(buttonContainer);
+Scenario('Button', (I) => {
+    const buttonSelector = '.uir-button';
+
+    I.searchWithinIframe(KIND, 'Overview', () => {
+        I.seeNumberOfElements(buttonSelector, 7);
+        I.see('CUSTOM LABEL', buttonSelector);
+        I.seeNumberOfElements('.uir-button--disabled', 1);
+        I.seeNumberOfElements('.uir-button--active', 1);
+        I.seeNumberOfElements('.uir-button--clear', 1);
+        I.seeNumberOfElements('.uir-button--primary', 1);
+        I.seeNumberOfElements('.uir-button--icon-left', 1);
+    });
+});
+
+Scenario('Button Clickable', (I) => {
+    const buttonSelector = '.uir-button';
+
+    I.searchWithinIframe(KIND, 'Overview', () => {
+        I.click(buttonSelector);
         I.seeInPopup('it works!');
         I.acceptPopup();
     });
