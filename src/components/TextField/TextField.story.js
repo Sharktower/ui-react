96c80400450e7a986bf6482a8c97c7948c788af1
Matt Davies <matt@agencycore.co.uk>
2018-01-22 17:13:32 +0000

    :star: [MDU-52] add label, value, placeholder and ref to input

--- src/components/TextField/TextField.story.js
+++ src/components/TextField/TextField.story.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import TextField from './TextField';
 import storyWrapper from '../../storybook-addons/storyWrapper';
 
@@ -10,5 +10,25 @@ stories.add(
     storyWrapper(
         'TextField is text input field.',
         <TextField label="TextField" />,
+    ),
+);
+
+stories.add(
+    'Placholder',
+    storyWrapper(
+        'Placholder will appear when the input has focus.',
+        <TextField label="TextField" placeholder="your placeholder here" />,
+    ),
+);
+
+stories.add(
+    'Value',
+    storyWrapper(
+        'Value allows you to set an initial value.',
+        <TextField
+            label="TextField"
+            placeholder="your placeholder here"
+            value="Hello, Friend"
+        />,
     ),
 );
