e5a14605667f80ad01a1dd97474a40c4520d9dfc
Jakub Frej <jakub.frej@mudano.com>
2018-02-09 16:47:37 +0000

    :white_check_mark: [UR-23] Use test helpers in IconMore tests

--- src/components/Icon/IconMore.test.js
+++ src/components/Icon/IconMore.test.js
@@ -1,16 +1,8 @@
-/* global describe, expect, it, shallow */
-import React from 'react';
+/* global describe */
+import * as common from '../../../test/unit/commonTests';
 import IconMore from './IconMore';
 
 describe('IconMore', () => {
-    it('renders an SVG tag', () => {
-        const wrapper = shallow(<IconMore />);
-
-        expect(wrapper).to.have.tagName('svg');
-    });
-
-    it('has the appropriate classes', () => {
-        const icon = shallow(<IconMore />);
-        expect(icon).to.have.className('uir-icon uir-icon-more');
-    });
+    common.isConformant(IconMore, { tagName: 'svg' });
+    common.addsClassName(IconMore, 'uir-icon');
 });
