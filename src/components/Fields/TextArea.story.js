e85fa787ebe82181086e25c474ec7028c6a49520
Matt Davies <matt@agencycore.co.uk>
2018-02-20 10:14:19 +0000

    :bug: [UR-51] fix textarea overflow bug

--- src/components/Fields/TextArea.story.js
+++ src/components/Fields/TextArea.story.js
@@ -58,7 +58,11 @@ stories.add(
 stories.add(
     'Prevent Auto Resize',
     storyWrapper(
-        'preventAutoResize stops the textarea from resizing as the content changes.',
+        `
+preventAutoResize stops the textarea from resizing as the content changes.
+
+The example below has a fixed height of 200px.
+`,
         <TextArea
             label="Full width not resizeable"
             preventAutoResize
