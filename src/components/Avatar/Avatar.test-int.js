74f7b7fc8409d359148e1e6a33c699c49d843688
Matt Davies <matt@agencycore.co.uk>
2018-04-30 15:05:43 +0100

    :white_check_mark: add accidentally removed Avatar int test

---
+++ src/components/Avatar/Avatar.test-int.js
@@ -0,0 +1,27 @@
+/* global Feature, Scenario */
+
+const KIND = 'Avatar.Avatar';
+
+Feature('Avatar');
+
+Scenario('Storybook Documentation', (I) => {
+    I.searchWithinIframe(KIND, 'Overview', () => {
+        I.see('Avatar: Avatar');
+        I.see('Overview', 'h2');
+        I.see('Example', 'h3');
+        I.see('Source Code', 'h3');
+        I.seeElement('code');
+        I.see('Props', 'h3');
+        I.seeElement('table');
+        I.see('property');
+        I.see('value');
+        I.see('required');
+        I.see('default');
+    });
+});
+
+Scenario('Avatar', (I) => {
+    I.searchWithinIframe(KIND, 'Overview', () => {
+        I.seeElement('.uir-avatar');
+    });
+});
