c4a93e42665b15b0456a27abd0ea7c3eae206031
Jakub Frej <jakub.frej@mudano.com>
2018-06-20 14:31:22 +0100

    :star: [UR-84] Add test for title prop

--- src/components/Icon/IconCalendar.test.js
+++ src/components/Icon/IconCalendar.test.js
@@ -1,8 +1,17 @@
-/* global describe */
+/* global describe, expect, it, shallow */
+import React from 'react';
 import * as common from '../../../test/unit/commonTests';
 import IconCalendar from './IconCalendar';
 
 describe('IconCalendar', () => {
     common.isConformant(IconCalendar, { tagName: 'svg' });
     common.addsClassName(IconCalendar, 'uir-icon');
+
+    it('sets title', () => {
+        const wrapper = shallow((
+            <IconCalendar title="Test title" />
+        ));
+
+        expect(wrapper.find('title')).to.have.text('Test title');
+    });
 });
