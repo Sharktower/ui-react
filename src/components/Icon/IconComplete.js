ed05ac2a7d2cfc30b0821e7a98eaf03a7e4989fb
Willian Krause <willian.krause@mudano.com>
2018-07-26 09:00:34 -0300

    Fix icon sizes

--- src/components/Icon/IconComplete.js
+++ src/components/Icon/IconComplete.js
@@ -29,7 +29,7 @@ const IconComplete = ({
         <svg
             className={cx('uir-icon uir-icon-status', className)}
             style={style}
-            viewBox="0 0 24 24"
+            viewBox="0 0 12 12"
             aria-labelledby={iconTitleId}
         >
             <title id={iconTitleId}>{title}</title>
