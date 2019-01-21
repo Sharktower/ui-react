741f8478f74d6c763d681bb0c295d226f8a72fd4
Jakub Frej <jakub.frej@mudano.com>
2019-01-21 00:08:37 +0000

    :star: [UR-101] Add title prop to IconMore

--- src/components/Icon/IconMore.test.js
+++ src/components/Icon/IconMore.test.js
@@ -3,6 +3,5 @@ import * as common from '../../../test/unit/commonTests';
 import IconMore from './IconMore';
 
 describe('IconMore', () => {
-    common.isConformant(IconMore, { tagName: 'svg' });
-    common.addsClassName(IconMore, 'uir-icon');
+    common.isConformantIcon(IconMore);
 });
