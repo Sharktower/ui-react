2c6f4fcde5d8fee1ba91ad38b2bd37b5f6abdd04
Jakub Frej <jakub.frej@mudano.com>
2019-01-21 00:07:11 +0000

    :star: [UR-101] Add icon title prop tests for IconEpic

--- src/components/Icon/IconEpic.test.js
+++ src/components/Icon/IconEpic.test.js
@@ -3,6 +3,5 @@ import * as common from '../../../test/unit/commonTests';
 import IconEpic from './IconEpic';
 
 describe('IconEpic', () => {
-    common.isConformant(IconEpic, { tagName: 'svg' });
-    common.addsClassName(IconEpic, 'uir-icon');
+    common.isConformantIcon(IconEpic);
 });
