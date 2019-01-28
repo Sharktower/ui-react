952a70e141f6565a6ba63fe79f77cc5bb02e35da
Raphael Ferreira <raphaelbsferreira@gmail.com>
2019-01-28 17:19:27 -0200

    add prop gradient to IconCompleted

--- src/components/Icon/IconComplete.js
+++ src/components/Icon/IconComplete.js
@@ -11,22 +11,30 @@ const propTypes = {
     className: PropTypes.string,
     style: StyleObjectPropType,
     title: PropTypes.string,
+    gradient: PropTypes.shape({
+        from: PropTypes.string.isRequired,
+        to: PropTypes.string.isRequired,
+    }),
 };
 
 const defaultProps = {
     className: null,
     style: null,
     title: 'Complete Icon',
+    gradient: null,
 };
 
 const IconComplete = ({
     className,
     style,
     title,
+    gradient,
     ...props
 }) => {
     lastInstanceId += 1;
     const iconTitleId = `icon-complete-title-${lastInstanceId}`;
+    const gradientId = `icon-complete-gradient-${lastInstanceId}`;
+
     return (
         <svg
             className={cx('uir-icon uir-icon-complete', className)}
@@ -35,12 +43,23 @@ const IconComplete = ({
             aria-labelledby={iconTitleId}
             {...proxyDataProps(props)}
         >
+            <defs>
+                {
+                    gradient &&
+                    <linearGradient id={gradientId} x1="-50%" x2="100%" gradientTransform="rotate(-40)">
+                        <stop offset="0%" stopColor={gradient.from} />
+                        <stop offset="60%" stopColor={gradient.to} />
+                    </linearGradient>
+                }
+            </defs>
             <title id={iconTitleId}>{title}</title>
             <path
+                style={{ fill: `url(#${gradientId})` }}
                 className="uir-icon-path"
                 d="M18.4 5.2zM18.9 10.3c.6 2.8-.3 5.7-2.4 7.6-3.3 3-8.4 2.8-11.4-.5s-2.8-8.3.5-11.3S14 3.3 17 6.6l1.4-1.4c-2.7-2.9-6.9-3.9-10.7-2.6-5.2 1.8-8 7.5-6.2 12.7 1.8 5.2 7.6 8 12.8 6.2s8-7.5 6.2-12.7l-1.6 1.5z"
             />
             <path
+                style={{ fill: `url(#${gradientId})` }}
                 className="uir-icon-path"
                 d="M21.6 3.5l-10 9.9-4.2-4.3-1.5 1.5 4.3 4.2.1.1 1.3 1.3L23 4.9z"
             />
