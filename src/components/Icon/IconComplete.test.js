a90cce924e9817b029049c14870395cf47874eeb
Raphael Ferreira <raphaelbsferreira@gmail.com>
2019-01-28 17:41:47 -0200

    add unit tests for gradient prop

--- src/components/Icon/IconComplete.test.js
+++ src/components/Icon/IconComplete.test.js
@@ -1,7 +1,18 @@
-/* global describe */
+/* global describe it shallow expect */
+import React from 'react';
 import * as common from '../../../test/unit/commonTests';
 import IconComplete from './IconComplete';
 
 describe('IconComplete', () => {
     common.isConformantIcon(IconComplete);
+
+    it("don't set gradient", () => {
+        const wrapper = shallow(<IconComplete />);
+        expect(wrapper.find('linearGradient')).to.not.exist();
+    });
+
+    it('set gradient', () => {
+        const wrapper = shallow(<IconComplete gradient={{ from: '#662483', to: '#af1c63' }} />);
+        expect(wrapper.find('linearGradient')).to.exist();
+    });
 });
